84e33a482aa9d0f264ff7b6cd006d07c4d3251d1
Stephen Schuez <stephen.schuez@gmail.com>
2022-01-21 18:04:20 +0100

    added lewagon's file and added composer's file for inspiration

--- devise.rb
+++ devise.rb
@@ -4,3 +4,11 @@
 # -m https://github.com/sschuez/rails-template/blob/main/devise.rb \
 # CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
 
+gem 'devise'
+
+run bundle 'install'
+
+generate(:scaffold, 'user', 'admin:boolean')
+
+rails_command('generate devise:install')
+rails_command('generate devise User')
