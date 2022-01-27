df15f80e8349b2c4592c9d63d3284c43812cec97
Stephen Schuez <stephen.schuez@gmail.com>
2022-01-27 13:16:49 +0100

    changed stylesheet to include adjusted applicaiton.scss.sass file

--- devise.rb
+++ devise.rb
@@ -8,132 +8,269 @@ run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 
 # GEMFILE
 ########################################
-inject_into_file 'Gemfile', before: 'group :development, :test do' do
-  <<~RUBY
-    gem 'devise'
-    # gem 'font-awesome-sass'
-    gem 'simple_form'
-    gem 'cssbundling-rails'
-  RUBY
+# inject_into_file 'Gemfile', before: 'group :development, :test do' do
+#   <<~RUBY
+#     gem 'devise'
+#     # gem 'font-awesome-sass'
+#     gem 'simple_form'
+#     gem 'cssbundling-rails'
+#   RUBY
+# end
+
+def rails_version
+  @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
 end
 
-inject_into_file 'Gemfile', after: 'group :development, :test do' do
-  <<-RUBY
-  gem 'pry-byebug'
-  gem 'pry-rails'
-  # gem 'dotenv-rails'
-  RUBY
+def rails_6_or_newer?
+  Gem::Requirement.new(">= 6.0.0.alpha").satisfied_by? rails_version
 end
 
-# Dev environment
-########################################
-gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+def add_gems
+  gem 'cssbundling-rails'
+  gem 'devise'
+  gem 'pundit'
+  gem 'jsbundling-rails'
+  gem 'simple_form'
+end
 
+# inject_into_file 'Gemfile', after: 'group :development, :test do' do
+#   <<-RUBY
+#   gem 'pry-byebug'
+#   gem 'pry-rails'
+#   # gem 'dotenv-rails'
+#   RUBY
+# end
 
-
-
-
-
-
-########################################
-# AFTER BUNDLE
-########################################
-after_bundle do
-
-  # Assets
-  ########################################
-  generate('rails css:install:sass')
+def add_users
+  route "root to: 'home#index'"
+  generate "devise:install"
   
-  run 'rm -rf vendor'
+  # Configure Devise to handle TURBO_STREAM requests like HTML requests
+  inject_into_file "config/initializers/devise.rb", "  config.navigational_formats = ['/', :html, :turbo_stream]", after: "Devise.setup do |config|\n"
+  
+  inject_into_file 'config/initializers/devise.rb', after: "# frozen_string_literal: true\n" do <<~EOF
+    class TurboFailureApp < Devise::FailureApp
+      def respond
+        if request_format == :turbo_stream
+          redirect
+        else
+          super
+        end
+      end
+  
+      def skip_format?
+        %w(html turbo_stream */*).include? request_format.to_s
+      end
+    end
+  EOF
+  end
+
+  inject_into_file 'config/initializers/devise.rb', after: "# ==> Warden configuration\n" do <<-EOF
+    config.warden do |manager|
+      manager.failure_app = TurboFailureApp
+    end
+    EOF
+  end
+
+  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
+  generate :devise, "User", "admin:boolean"
+
+  # Set admin default to false
+  in_root do
+    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ":admin, default: false"
+  end
+
+  if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
+    gsub_file "config/initializers/devise.rb", /  # config.secret_key = .+/, "  config.secret_key = Rails.application.credentials.secret_key_base"
+  end
+end
+
+def add_authorization
+  generate 'pundit:install'
+end
+
+def add_jsbundling
+  rails_command "javascript:install:esbuild"
+end
+
+def add_javascript
+  run "yarn add local-time esbuild-rails trix @hotwired/stimulus @hotwired/turbo-rails @rails/activestorage @rails/ujs @rails/request.js"
+end
+
+def add_sass
+  rails_command "css:install:sass"
+end
+
+def copy_templates
+  # run 'rm -rf vendor'
   run 'curl -L https://github.com/sschuez/rails-template/raw/main/stylesheets.zip > stylesheets.zip'
-  run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/stylesheets app/assets/stylesheets'
+  run 'rm app/assets/stylesheets/application.sass.scss'
+  run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip' #&& mv app/assets/stylesheets app/assets/stylesheets'
+  run 'rm -r app/assets/__MACOSX'
+end
 
-
-
-  # Generators: db + simple form + pages controller
-  ########################################
-  rails_command 'db:drop db:create db:migrate'
-  generate('simple_form:install', '--bootstrap')
-  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+def add_simple_form
+  rails_command "simple_form:install --bootstrap"
 
   # Replace simple form initializer to work with Bootstrap 5
   run 'curl -L https://raw.githubusercontent.com/heartcombo/simple_form-bootstrap/main/config/initializers/simple_form_bootstrap.rb > config/initializers/simple_form_bootstrap.rb'
 
-  # Routes
-  ########################################
-  route "root to: 'pages#home'"
-
-  # Git ignore
-  ########################################
-  append_file '.gitignore', <<~TXT
-    # Ignore .env file containing credentials.
-    .env*
-    # Ignore Mac and Linux file system files
-    *.swp
-    .DS_Store
-  TXT
-
-  # Devise install + user
-  ########################################
-  generate('devise:install')
-  generate('devise', 'User')
-
-  # App controller
-  ########################################
-  run 'rm app/controllers/application_controller.rb'
-  file 'app/controllers/application_controller.rb', <<~RUBY
-    class ApplicationController < ActionController::Base
-    #{  "protect_from_forgery with: :exception\n" if Rails.version < "5.2"}  before_action :authenticate_user!
-    end
-  RUBY
-
-  # migrate + devise views
-  ########################################
-  rails_command 'db:migrate'
-  generate('devise:views')
-
-  # Pages Controller
-  ########################################
-  run 'rm app/controllers/pages_controller.rb'
-  file 'app/controllers/pages_controller.rb', <<~RUBY
-    class PagesController < ApplicationController
-      skip_before_action :authenticate_user!, only: [ :home ]
-
-      def home
-      end
-    end
-  RUBY
-
-  # Environments
-  ########################################
-  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
-  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
-
-  # # Webpacker / Yarn
-  # ########################################
-  # run 'yarn add bootstrap @popperjs/core'
-  # run "rails webpacker:install:stimulus"
-  # append_file 'app/javascript/packs/application.js', <<~JS
-  #   import "bootstrap"
-  # JS
-
-  # inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
-  #   <<~JS
-  #     // Preventing Babel from transpiling NodeModules packages
-  #     environment.loaders.delete('nodeModules');
-  #   JS
-  # end
-
-  # # Dotenv
-  # ########################################
-  # run 'touch .env'
-
-  # # Rubocop
-  # ########################################
-  # run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
-
-  # Git
-  ########################################
-  git add: '.'
-  git commit: "-m 'Initial commit with devise template from Stephen's template'"
 end
+
+def add_esbuild_script
+  build_script = "node esbuild.config.js"
+
+  if (`npx -v`.to_f < 7.1 rescue "Missing")
+    say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
+  else
+    run %(npm set-script build "#{build_script}")
+  end
+end
+
+unless rails_6_or_newer?
+  puts "Please use Rails 6.0 or newer to create a Jumpstart application"
+end
+
+# Main setup
+add_gems
+
+after_bundle do
+  add_users
+  add_authorization
+  add_jsbundling
+  add_javascript
+  
+  add_sass
+  copy_templates
+  add_simple_form
+  add_esbuild_script
+  
+  rails_command "active_storage:install"
+  
+  # Commit everything to git
+  unless ENV["SKIP_GIT"]
+    git :init
+    git add: "."
+    # git commit will fail if user.email is not configured
+    begin
+      git commit: %( -m 'Initial commit' )
+    rescue StandardError => e
+      puts e.message
+    end
+  end
+
+end
+
+# # Dev environment
+# ########################################
+# gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
+
+
+
+
+
+
+
+# ########################################
+# # AFTER BUNDLE
+# ########################################
+# after_bundle do
+
+#   # Assets
+#   ########################################
+#   generate('css:install:sass')
+  
+#   run 'rm -rf vendor'
+#   run 'curl -L https://github.com/sschuez/rails-template/raw/main/stylesheets.zip > stylesheets.zip'
+#   run 'unzip stylesheets.zip -d app/assets && rm stylesheets.zip && mv app/assets/stylesheets app/assets/stylesheets'
+
+
+
+#   # Generators: db + simple form + pages controller
+#   ########################################
+#   rails_command 'db:drop db:create db:migrate'
+#   generate('simple_form:install', '--bootstrap')
+#   generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+#   # Replace simple form initializer to work with Bootstrap 5
+#   run 'curl -L https://raw.githubusercontent.com/heartcombo/simple_form-bootstrap/main/config/initializers/simple_form_bootstrap.rb > config/initializers/simple_form_bootstrap.rb'
+
+#   # Routes
+#   ########################################
+#   route "root to: 'pages#home'"
+
+#   # Git ignore
+#   ########################################
+#   append_file '.gitignore', <<~TXT
+#     # Ignore .env file containing credentials.
+#     .env*
+#     # Ignore Mac and Linux file system files
+#     *.swp
+#     .DS_Store
+#   TXT
+
+#   # Devise install + user
+#   ########################################
+#   generate('devise:install')
+#   generate('devise', 'User')
+
+#   # App controller
+#   ########################################
+#   run 'rm app/controllers/application_controller.rb'
+#   file 'app/controllers/application_controller.rb', <<~RUBY
+#     class ApplicationController < ActionController::Base
+#     #{  "protect_from_forgery with: :exception\n" if Rails.version < "5.2"}  before_action :authenticate_user!
+#     end
+#   RUBY
+
+#   # migrate + devise views
+#   ########################################
+#   rails_command 'db:migrate'
+#   generate('devise:views')
+
+#   # Pages Controller
+#   ########################################
+#   run 'rm app/controllers/pages_controller.rb'
+#   file 'app/controllers/pages_controller.rb', <<~RUBY
+#     class PagesController < ApplicationController
+#       skip_before_action :authenticate_user!, only: [ :home ]
+
+#       def home
+#       end
+#     end
+#   RUBY
+
+#   # Environments
+#   ########################################
+#   environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+#   environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
+
+#   # # Webpacker / Yarn
+#   # ########################################
+#   # run 'yarn add bootstrap @popperjs/core'
+#   # run "rails webpacker:install:stimulus"
+#   # append_file 'app/javascript/packs/application.js', <<~JS
+#   #   import "bootstrap"
+#   # JS
+
+#   # inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+#   #   <<~JS
+#   #     // Preventing Babel from transpiling NodeModules packages
+#   #     environment.loaders.delete('nodeModules');
+#   #   JS
+#   # end
+
+#   # # Dotenv
+#   # ########################################
+#   # run 'touch .env'
+
+#   # # Rubocop
+#   # ########################################
+#   # run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
+
+#   # Git
+#   ########################################
+#   git add: '.'
+#   git commit: "-m 'Initial commit with devise template from Stephen's template'"
+# end
---
+++ gorails.rb
@@ -0,0 +1,276 @@
+require "fileutils"
+require "shellwords"
+
+# Copied from: https://github.com/mattbrictson/rails-template
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("jumpstart-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/excid3/jumpstart.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{jumpstart/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
+
+def rails_version
+  @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
+end
+
+def rails_6_or_newer?
+  Gem::Requirement.new(">= 6.0.0.alpha").satisfied_by? rails_version
+end
+
+def add_gems
+  gem 'cssbundling-rails'
+  gem 'devise', '~> 4.8', '>= 4.8.0'
+  gem 'friendly_id', '~> 5.4'
+  gem 'image_processing'
+  gem 'jsbundling-rails'
+  gem 'madmin'
+  gem 'name_of_person', '~> 1.1'
+  gem 'noticed', '~> 1.4'
+  gem 'omniauth-facebook', '~> 8.0'
+  gem 'omniauth-github', '~> 2.0'
+  gem 'omniauth-twitter', '~> 1.4'
+  gem 'pretender', '~> 0.3.4'
+  gem 'pundit', '~> 2.1'
+  gem 'sidekiq', '~> 6.2'
+  gem 'sitemap_generator', '~> 6.1'
+  gem 'whenever', require: false
+  gem 'responders', github: 'heartcombo/responders', branch: 'main'
+end
+
+def set_application_name
+  # Add Application Name to Config
+  environment "config.application_name = Rails.application.class.module_parent_name"
+
+  # Announce the user where they can change the application name in the future.
+  puts "You can change application name inside: ./config/application.rb"
+end
+
+def add_users
+  route "root to: 'home#index'"
+  generate "devise:install"
+
+  # Configure Devise to handle TURBO_STREAM requests like HTML requests
+  inject_into_file "config/initializers/devise.rb", "  config.navigational_formats = ['/', :html, :turbo_stream]", after: "Devise.setup do |config|\n"
+
+  inject_into_file 'config/initializers/devise.rb', after: "# frozen_string_literal: true\n" do <<~EOF
+    class TurboFailureApp < Devise::FailureApp
+      def respond
+        if request_format == :turbo_stream
+          redirect
+        else
+          super
+        end
+      end
+
+      def skip_format?
+        %w(html turbo_stream */*).include? request_format.to_s
+      end
+    end
+  EOF
+  end
+
+  inject_into_file 'config/initializers/devise.rb', after: "# ==> Warden configuration\n" do <<-EOF
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
+  EOF
+  end
+
+  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
+  generate :devise, "User", "first_name", "last_name", "announcements_last_read_at:datetime", "admin:boolean"
+
+  # Set admin default to false
+  in_root do
+    migration = Dir.glob("db/migrate/*").max_by{ |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ":admin, default: false"
+  end
+
+  if Gem::Requirement.new("> 5.2").satisfied_by? rails_version
+    gsub_file "config/initializers/devise.rb", /  # config.secret_key = .+/, "  config.secret_key = Rails.application.credentials.secret_key_base"
+  end
+
+  inject_into_file("app/models/user.rb", "omniauthable, :", after: "devise :")
+end
+
+def add_authorization
+  generate 'pundit:install'
+end
+
+def add_jsbundling
+  rails_command "javascript:install:esbuild"
+end
+
+def add_javascript
+  run "yarn add local-time esbuild-rails trix @hotwired/stimulus @hotwired/turbo-rails @rails/activestorage @rails/ujs @rails/request.js"
+end
+
+def copy_templates
+  remove_file "app/assets/stylesheets/application.css"
+  remove_file "app/javascript/controllers/index.js"
+  remove_file "Procfile.dev"
+
+  copy_file "Procfile"
+  copy_file "Procfile.dev"
+  copy_file ".foreman"
+  copy_file "esbuild.config.js"
+  copy_file "app/javascript/controllers/index.js"
+
+  directory "app", force: true
+  directory "config", force: true
+  directory "lib", force: true
+
+  # route "get '/terms', to: 'home#terms'"
+  # route "get '/privacy', to: 'home#privacy'"
+end
+
+def add_sidekiq
+  environment "config.active_job.queue_adapter = :sidekiq"
+
+  insert_into_file "config/routes.rb",
+    "require 'sidekiq/web'\n\n",
+    before: "Rails.application.routes.draw do"
+
+  content = <<~RUBY
+                authenticate :user, lambda { |u| u.admin? } do
+                  mount Sidekiq::Web => '/sidekiq'
+
+                  namespace :madmin do
+                    resources :impersonates do
+                      post :impersonate, on: :member
+                      post :stop_impersonating, on: :collection
+                    end
+                  end
+                end
+            RUBY
+  insert_into_file "config/routes.rb", "#{content}\n", after: "Rails.application.routes.draw do\n"
+end
+
+def add_announcements
+  generate "model Announcement published_at:datetime announcement_type name description:text"
+  route "resources :announcements, only: [:index]"
+end
+
+def add_notifications
+  route "resources :notifications, only: [:index]"
+end
+
+def add_multiple_authentication
+  insert_into_file "config/routes.rb", ', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }', after: "  devise_for :users"
+
+  generate "model Service user:references provider uid access_token access_token_secret refresh_token expires_at:datetime auth:text"
+
+  template = """
+  env_creds = Rails.application.credentials[Rails.env.to_sym] || {}
+  %i{ facebook twitter github }.each do |provider|
+    if options = env_creds[provider]
+      config.omniauth provider, options[:app_id], options[:app_secret], options.fetch(:options, {})
+    end
+  end
+  """.strip
+
+  insert_into_file "config/initializers/devise.rb", "  " + template + "\n\n", before: "  # ==> Warden configuration"
+end
+
+def add_whenever
+  run "wheneverize ."
+end
+
+def add_friendly_id
+  generate "friendly_id"
+  insert_into_file( Dir["db/migrate/**/*friendly_id_slugs.rb"].first, "[5.2]", after: "ActiveRecord::Migration")
+end
+
+def add_sitemap
+  rails_command "sitemap:install"
+end
+
+def add_bootstrap
+  rails_command "css:install:bootstrap"
+end
+
+def add_announcements_css
+  insert_into_file 'app/assets/stylesheets/application.bootstrap.scss', '@import "jumpstart/announcements";'
+end
+
+def add_esbuild_script
+  build_script = "node esbuild.config.js"
+
+  if (`npx -v`.to_f < 7.1 rescue "Missing")
+    say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
+  else
+    run %(npm set-script build "#{build_script}")
+  end
+end
+
+unless rails_6_or_newer?
+  puts "Please use Rails 6.0 or newer to create a Jumpstart application"
+end
+
+# Main setup
+add_template_repository_to_source_path
+
+add_gems
+
+after_bundle do
+  set_application_name
+  add_users
+  add_authorization
+  add_jsbundling
+  add_javascript
+  add_announcements
+  add_notifications
+  add_multiple_authentication
+  add_sidekiq
+  add_friendly_id
+
+  copy_templates
+  add_whenever
+  add_sitemap
+  add_bootstrap
+  add_announcements_css
+  add_esbuild_script
+
+  rails_command "active_storage:install"
+
+  # Commit everything to git
+  unless ENV["SKIP_GIT"]
+    git :init
+    git add: "."
+    # git commit will fail if user.email is not configured
+    begin
+      git commit: %( -m 'Initial commit' )
+    rescue StandardError => e
+      puts e.message
+    end
+  end
+
+  say
+  say "Jumpstart app successfully created!", :blue
+  say
+  say "To get started with your new app:", :green
+  say "  cd #{original_app_name}"
+  say
+  say "  # Update config/database.yml with your database credentials"
+  say
+  say "  rails db:create db:migrate"
+  say "  rails g noticed:model"
+  say "  rails g madmin:install # Generate admin dashboards"
+  say "  gem install foreman"
+  say "  bin/dev"
+end
