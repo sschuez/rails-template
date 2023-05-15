a914bb1ba9af8eb132ae1c3100adb92a3aaac838
Stephen Schuez <stephen.schuez@gmail.com>
2023-05-15 16:14:25 +0900

    Remove duplicate code

--- template_bootstrap.rb
+++ template_bootstrap.rb
@@ -633,7 +633,7 @@ def layouts
 end
 
 def set_up_rspec
-  rails_command "./bin/bundle binstubs rspec-core"
+  run "bundle binstubs rspec-core"
   # Make all necesarry directories
   system 'mkdir', '-p', 'spec/support'
   system 'mkdir', '-p', 'spec/features'
@@ -643,22 +643,7 @@ def set_up_rspec
   # Adjust rails_helper.rb
   insert_into_file 'spec/rails_helper.rb', before: "RSpec.configure do |config|\n" do
     <<-RUBY
-      require 'spec_helper'
-      ENV['RAILS_ENV'] ||= 'test'
-      require_relative '../config/environment'
-      # Prevent database truncation if the environment is production
-      abort("The Rails environment is running in production mode!") if Rails.env.production?
-      require 'rspec/rails'
-
-      Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
-
-      # Checks for pending migrations and applies them before tests are run.
-      # If you are not using ActiveRecord, you can remove these lines.
-      begin
-        ActiveRecord::Migration.maintain_test_schema!
-      rescue ActiveRecord::PendingMigrationError => e
-        abort e.to_s.strip
-      end
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
     RUBY
   end
   
