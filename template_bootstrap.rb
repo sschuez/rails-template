1ea56efbc163011ab72554e0b0d20f067e66ffca
Stephen <stephens@hey.com>
2023-12-14 16:51:52 +0100

    Minor changes

--- template_bootstrap.rb
+++ template_bootstrap.rb
@@ -72,7 +72,7 @@ def add_dartsass_rails
 @use "utilities/margins";
 
 // External Libraries
-@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
+// @import url("https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css");
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css");')
 end
 
